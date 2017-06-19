6b94020f88b7f55c01475b2ed60a3029798130f9
唐强 <tangqiang@tangqiangdeMacBook-Pro.local>
2017-06-19 18:44:00 +0800

    新增Person类

---
+++ NewMoMo/NewMoMo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  NewMoMo
+//
+//  Created by 唐强 on 2017/6/19.
+//  Copyright © 2017年 唐强. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ NewMoMo/NewMoMo/Person.m
@@ -0,0 +1,15 @@
+//
+//  Person.m
+//  NewMoMo
+//
+//  Created by 唐强 on 2017/6/19.
+//  Copyright © 2017年 唐强. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+
+
+@end
